6dca25412c3c0ef94ce78f1d411d962cc8fb9769
Ubuntu <ubuntu@ip-172-31-25-38.ap-northeast-1.compute.internal>
2026-05-31 09:39:35 +0000

    フロイド循環検出法による連結リストの循環を検出するアルゴリズム

---
+++ 141-arai60-linked-list-cycle/floyd-cycle-finding.c
@@ -0,0 +1,17 @@
+typedef struct ListNode ListNode;
+
+bool hasCycle(struct ListNode *head){
+	ListNode *fast = head;
+	ListNode *slow = head;
+
+	while(fast != NULL && fast->next != NULL){
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (fast == slow){
+			return true;
+		}
+	}
+
+	return false;
+}
