6de33fe0e8dfa29fd32885e12a8fb50e61814672
Ubuntu <ubuntu@ip-172-31-25-38.ap-northeast-1.compute.internal>
2026-06-21 03:03:35 +0000

    learning merge sort

---
+++ 0373-k-pairs-with-smallest-sums/merge-sort.c
@@ -0,0 +1,24 @@
+void devide(){
+}
+
+void merge(){
+}
+
+void mergeSort(int *arr, int left, int right){
+	int m
+	mergeSort(int *arr, int middle, int right);
+	mergeSort(int *arr, int left, int middle)
+
+	return;
+}
+
+
+int main(){
+	int arr[10] = {
+		10, 3, 0, 12, 111, 294, 1, 8, 23, 99
+	};
+
+	mergeSort(arr, 0, 0);
+
+	return;
+}
